5eadbd3fcbfb69a4171ac8b6c55ea41099314ae8
IsacSnecker <isac.snecker@hotmail.com>
2024-01-31 20:08:30 +0100

    Fixed PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,42 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+
+    private DefaultPlayerInputs input = null;
+
+    private Vector2 inputVector = Vector2.zero;
     public float walkSpeed = 5f;
-    Vector2 moveInput;
-    public bool IsMoving { get; private set; }
+    public float jumpHeight = 6f;
+
     Rigidbody2D rb;
 
-    private void Awake()
-    {
+    private void Awake() {
+        input = new DefaultPlayerInputs();
         rb = GetComponent<Rigidbody2D>();
     }
+
+    private void OnEnable() {
+        input.Enable();
+        input.Player.Move.performed += OnMovementPerformed;
+        input.Player.Move.canceled += OnMovementCancelled;
+        input.Player.Jump.performed += OnJumpPerformed;
+    }
+
+    private void OnDisable() {
+        input.Disable();
+        input.Player.Move.performed -= OnMovementPerformed;
+        input.Player.Jump.performed -= OnJumpPerformed;
+
+    }
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+    void FixedUpdate() {
+        rb.velocity = new Vector2(inputVector.x * walkSpeed, rb.velocity.y);
     }
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-
-        IsMoving = moveInput != Vector2.zero;
-
+    private void OnMovementPerformed(InputAction.CallbackContext context) {
+        Debug.Log("Moved");
+        inputVector = context.ReadValue<Vector2>();
     }
+
+    private void OnMovementCancelled(InputAction.CallbackContext context) {
+        Debug.Log("Stopped Moving");
+        inputVector = Vector2.zero;
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext context) {
+        Debug.Log("Jumped");
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+
 }
