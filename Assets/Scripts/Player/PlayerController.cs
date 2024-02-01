18cb55478c8227f381734ace7bccc9f24cc3c476
IsacSnecker <isac.snecker@hotmail.com>
2024-02-01 14:24:24 +0100

    Separated JumpController.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,15 +8,11 @@ using UnityEngine.Rendering;
 public class PlayerController : MonoBehaviour
 {
 
-    Animator animator;
+    private Animator animator;
 
     public float walkSpeed = 5f;
-    public float jumpHeight = 6f;
-    public int maxJumps = 2;
-    public int jumpCount = 0;
 
     private DefaultPlayerInputs input = null;
-    private TouchingDirections touchingDirections;
 
     private SpriteRenderer spriteRenderer;
 
@@ -27,7 +23,6 @@ public class PlayerController : MonoBehaviour
     private void Awake() {
         input = new DefaultPlayerInputs();
         rb = GetComponent<Rigidbody2D>();
-        touchingDirections = GetComponent<TouchingDirections>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
@@ -36,36 +31,16 @@ public class PlayerController : MonoBehaviour
         input.Enable();
         input.Player.Move.performed += OnMovementPerformed;
         input.Player.Move.canceled += OnMovementCancelled;
-        input.Player.Jump.performed += OnJumpPerformed;
     }
 
     private void OnDisable() {
         input.Disable();
         input.Player.Move.performed -= OnMovementPerformed;
-        input.Player.Jump.performed -= OnJumpPerformed;
-
     }
 
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void FixedUpdate() {
         rb.velocity = new Vector2(inputVector.x * walkSpeed, rb.velocity.y);
-
-        if (touchingDirections.IsGrounded) { jumpCount = 0; }
-
-
     }
 
     private void OnMovementPerformed(InputAction.CallbackContext context) {
@@ -87,14 +62,6 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Stopped Moving");
         inputVector = Vector2.zero;
         animator.SetBool("IsRunning", false);
-    }
-
-    private void OnJumpPerformed(InputAction.CallbackContext context) {
-        Debug.Log("Jumped");
-        if (jumpCount < maxJumps - 1) {
-             rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
-             jumpCount++;
-        }
     }
 
 }
