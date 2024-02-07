725e19ac4e89183ad3b930eeb5a9b65907fe0a4a
Huffla <dator343@gmail.com>
2024-02-07 17:12:21 +0100

    Added enemy

---
+++ Assets/Scripts/SkeletonMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonMovement : MonoBehaviour
+{
+
+    
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
