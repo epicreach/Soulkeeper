e10e3aa5d52750bfcd48e732dc48afcc76ff3561
JonCed <jonceder@student.chalmers.se>
2024-02-07 19:56:41 +0100

    Fiende rör sig och är arg

--- Assets/Scripts/Enemies/Grog.cs
+++ Assets/Scripts/Enemies/Grog.cs
@@ -12,25 +12,34 @@ public class Grog : MonoBehaviour
 
     private float attackCooldownTimer = Mathf.Infinity;
 
+    private EnemyPatrol enemyPatrol;
+
     private Animator animator;
 
     private void Awake(){
         animator = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
 
-    private void update()
+    private void Update()
     {
        attackCooldownTimer += Time.deltaTime;
 
         //attack if sees player
         if(seesPlayer())
         {
+            Debug.Log("attacked"); 
             if(attackCooldownTimer >= attackCooldown)
             {
                 // Attack
                 attackCooldownTimer = 0;
                 animator.SetTrigger("attack");
             }
+        }
+
+        if (enemyPatrol != null)
+        {
+            enemyPatrol.enabled = !seesPlayer();
         }
     }
 
@@ -39,6 +48,7 @@ public class Grog : MonoBehaviour
             Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
             0, Vector2.left, 0, playerLayer);
+        Debug.DrawRay(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, Vector2.left * range, Color.red);
         return hit.collider != null;
     }
 
