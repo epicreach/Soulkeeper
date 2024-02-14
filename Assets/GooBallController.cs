e16ca553a684e5e04dfdaa9c468fa14bcfc7644e
IsacSnecker <isac.snecker@hotmail.com>
2024-02-14 13:23:11 +0100

    Added goobals

---
+++ Assets/GooBallController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GooBallController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    Rigidbody2D rb;
+
+    float speed = 3.0f;
+    float damage = 10.0f;
+
+        Vector2 playerPosition;
+        Vector2 ballPosition;
+        Vector2 travelDirection;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+
+        playerPosition = player.transform.position;
+        ballPosition = this.transform.position;
+        travelDirection = new Vector2(playerPosition.x - ballPosition.x, 0);
+        travelDirection.Normalize();
+    }
+
+    void FixedUpdate() {
+
+        rb.velocity = travelDirection * speed;
+
+
+    }
+
+}
